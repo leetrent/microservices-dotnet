01284353a58f1cc0d6ba72c1bcae3483aa7871d7
LeeTrent <lee.trent.1@gmail.com>
2021-01-04 21:02:55 -0600

    Adds GetProducts method

--- MicroservicesApp/src/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ MicroservicesApp/src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Catalog.API.Controllers
 {
+    [Route("api/v1/[controller]")]
+    [ApiController]
     public class CatalogController : ControllerBase
     {
+        private readonly IProductRepository _repository;
+
+        public CatalogController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+
+        }
     }
+
+
+
 }
