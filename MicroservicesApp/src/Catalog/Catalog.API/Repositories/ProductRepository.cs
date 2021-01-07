58d6b8665f0ea19e71faa02c962a616e4d7770b0
LeeTrent <lee.trent.1@gmail.com>
2021-01-06 20:44:12 -0600

    Fixed bug in delete method

--- MicroservicesApp/src/Catalog/Catalog.API/Repositories/ProductRepository.cs
+++ MicroservicesApp/src/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -47,7 +47,7 @@ namespace Catalog.API.Repositories
 
         public async Task<bool> Delete(string id)
         {
-            FilterDefinition<Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Id, id);
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
             DeleteResult deleteResult = await _context.Products.DeleteOneAsync(filter);
             return (deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0);
         }
