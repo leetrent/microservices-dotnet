e1ce9f9a293ab8e9ea5fe1811b6c899746d522c8
LeeTrent <lee.trent.1@gmail.com>
2021-03-07 10:23:13 -0600

    Adds 'Checkout' method to BasketController in Basket.API package.

--- MicroservicesApp/src/Basket/Basket.API/Controllers/BasketController.cs
+++ MicroservicesApp/src/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,5 +1,9 @@
-﻿using Basket.API.Entities;
+﻿using AutoMapper;
+using Basket.API.Entities;
 using Basket.API.Repositories.Interfaces;
+using EventBusRabbitMQ.Common;
+using EventBusRabbitMQ.Events;
+using EventBusRabbitMQ.Producer;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -14,10 +18,14 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly EventBusRabbitMQProducer _eventBusProducer;
 
-        public BasketController(IBasketRepository repository)
-        {
+        public BasketController(IBasketRepository repository, IMapper mapper, EventBusRabbitMQProducer eventBusProducer)
+        { 
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _eventBusProducer = eventBusProducer ?? throw new ArgumentNullException(nameof(eventBusProducer));
         }
 
         [HttpGet]
@@ -40,6 +48,40 @@ namespace Basket.API.Controllers
         public async Task<IActionResult> DeleteBasket(string userName)
         {
             return Ok(await _repository.DeleteBasket(userName));
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            BasketCart basketCart = await _repository.GetBasket(basketCheckout.UserName);
+            if (basketCart == null)
+            {
+                return BadRequest();
+            }
+
+            bool deleteWasSuccessful = await _repository.DeleteBasket(basketCart.UserName);
+            if (deleteWasSuccessful == false)
+            {
+                return BadRequest();
+            }
+
+            BasketCheckoutEvent basketCheckoutEvent = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            basketCheckoutEvent.RequestId = Guid.NewGuid();
+            basketCheckoutEvent.TotalPrice = basketCart.TotalPrice;
+
+            try
+            {
+                _eventBusProducer.PublishBasketCheckout(EventBusConstants.BasketCheckoutQueue, basketCheckoutEvent);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Accepted();
         }
 
     }
