35d07f331e8d8b51fd92ae1f09033a956354f738
LeeTrent <lee.trent.1@gmail.com>
2021-03-19 08:43:57 -0500

    Wrote data migration and data seed logic (untested)

---
+++ MicroservicesApp/src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            try
+            {
+                orderContext.Database.Migrate();
+
+                if (await orderContext.Orders.AnyAsync() == false)
+                {
+                    orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception exception)
+            {
+                if (retryForAvailability < 3)
+                {
+                    retryForAvailability++;
+                    ILogger logger = loggerFactory.CreateLogger<OrderContextSeed>();
+                    logger.LogError(exception.Message);
+                    await SeedAsync(orderContext, loggerFactory, retryForAvailability);
+                }              
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() 
+                {
+                    UserName = "swn", 
+                    FirstName = "Mehmet", 
+                    LastName = "Ozkaya",
+                    EmailAddress = "ezozkme@gmail.com",
+                    AddressLine = "Bahcelievler",
+                    Country = "Turkey"
+                }
+            };
+        }
+    }
+}
